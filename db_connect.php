0bf57d7f9b8ae069f50421f633f161bcd799ceba
Teesha Gopall <chandikabye.gopall@umail.uom.ac.mu>
2026-03-06 17:24:30 +0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,4 @@
 <?php
-session_start();
-include 'includes/db_connect.php';
-?>
-<?php
 // Database configuration
 $host = 'localhost';         // or your server name
 $dbname = 'mauritius_guide'; // your database name
